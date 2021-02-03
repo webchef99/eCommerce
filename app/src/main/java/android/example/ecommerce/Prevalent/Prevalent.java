50b4f372068b8dc7937c62bd9ec18c8c1a10e918
IMPERIALLEGEND <tejaswawadekar@gmail.com>
2021-02-03 20:35:44 +0530

    Uploading files

---
+++ app/src/main/java/android/example/ecommerce/Prevalent/Prevalent.java
@@ -0,0 +1,11 @@
+package android.example.ecommerce.Prevalent;
+
+import android.example.ecommerce.Model.Users;
+
+public class Prevalent
+{
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
---
+++ app/src/main/java/android/example/ecommerce/ViewHolder/CartViewHolder.java
@@ -0,0 +1,37 @@
+package android.example.ecommerce.ViewHolder;
+
+import android.example.ecommerce.Interface.ItemClickListner;
+import android.example.ecommerce.R;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CartViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
+{
+
+    public TextView txtProductName, txtProductPrice, txtProductQuantity;
+    private ItemClickListner itemClickListner;
+
+    public CartViewHolder(@NonNull View itemView) {
+        super(itemView);
+        txtProductName = itemView.findViewById(R.id.cart_product_name);
+        txtProductPrice = itemView.findViewById(R.id.cart_product_price);
+        txtProductQuantity = itemView.findViewById(R.id.cart_product_quantity);
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        itemClickListner.onClick(view,getAdapterPosition(),false);
+
+
+
+    }
+
+    public void setItemClickListner(ItemClickListner itemClickListner) {
+        this.itemClickListner = itemClickListner;
+    }
+}
